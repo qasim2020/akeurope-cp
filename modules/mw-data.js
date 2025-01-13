aaa17030d42535972ac1111d9022ad9eeca05c69
Qasim Ali <qasimali24@gmail.com>
2025-01-13 17:39:02 +0100

    entry middle ware to make sure only paid valid subscriptions can be viewed.

--- modules/mw-data.js
+++ modules/mw-data.js
@@ -1,15 +1,53 @@
-const Project = require("../models/Project");
+const Project = require('../models/Project');
+const Order = require('../models/Order');
+const moment = require('moment');
 
 const oneProject = async (req, res, next) => {
-    let oneProject = await Project.findOne({slug: req.params.slug}).lean();
+    let oneProject = await Project.findOne({ slug: req.params.slug }).lean();
     req.oneProject = oneProject;
     next();
 };
 
 const allProjects = async (req, res, next) => {
-    let allProjects = await Project.find().lean();
+    let allProjects = await Project.find({ status: 'active' }).lean();
     req.allProjects = allProjects;
     next();
 };
 
-module.exports = { allProjects, oneProject };
+const entryExistsInPaidOrder = async (req, res, next) => {
+    try {
+        const entryId = req.params.entryId;
+
+        if (!entryId) {
+            throw new Error ('No entry id provided in request');
+        }
+
+        const order = await Order.findOne({
+            customerId: req.session.user._id,
+            'projects.entries.entryId': entryId,
+            status: 'paid',
+        });
+
+        if (!order) {
+            throw new Error ('Entry does not exist in any paid order');
+        };
+
+        for (const project of order.projects) {
+            for (const entry of project.entries) {
+                if (entry.entryId.toString() === entryId.toString()) {
+                    const projectExpirationDate = moment(order.createdAt).add(project.months, 'months');
+                    if (moment().isAfter(projectExpirationDate)) {
+                        throw new Error ('Invoice has expired to view linked beneficiaries!');
+                    }
+                }
+            }
+        }
+
+        next();
+    } catch (error) {
+        console.log(error);
+        return res.status(404).render("error", {heading: "Unauthorized", error: error});
+    }
+};
+
+module.exports = { allProjects, oneProject, entryExistsInPaidOrder };
--- routes/entryRoutes.js
+++ routes/entryRoutes.js
@@ -1,14 +1,14 @@
 const express = require('express');
 const router = express.Router();
 const { authenticate, authorize } = require('../modules/auth');
-const { allProjects } = require("../modules/mw-data");
+const { allProjects, entryExistsInPaidOrder } = require("../modules/mw-data");
 const entryController = require('../controllers/entryController');
 
-router.get('/entry/:entryId/project/:slug', authenticate, authorize("viewEntry"), allProjects, entryController.entry);
+router.get('/entry/:entryId/project/:slug', authenticate, authorize("viewEntry"), entryExistsInPaidOrder, allProjects, entryController.entry);
 router.get('/getEntryData/:slug', authenticate, authorize("viewEntry"), entryController.getData);
 
-router.get('/getSingleEntryData/:entryId/project/:slug', authenticate, authorize("editEntry"), entryController.getSingleEntryData);
-router.get('/getSingleEntryLogs/:entryId/project/:slug', authenticate, authorize("editEntry"), entryController.getSingleEntryLogs);
+router.get('/getSingleEntryData/:entryId/project/:slug', authenticate, authorize("editEntry"), entryExistsInPaidOrder, entryController.getSingleEntryData);
+router.get('/getSingleEntryLogs/:entryId/project/:slug', authenticate, authorize("editEntry"), entryExistsInPaidOrder, entryController.getSingleEntryLogs);
 
 router.get('/getPaginatedEntriesForDraftOrder/:slug', authenticate, authorize('viewEntry'), entryController.getPaginatedEntriesForDraftOrder);
 router.get('/getPaginatedEntriesForOrderPage/:slug', authenticate, authorize('viewEntry'), entryController.getPaginatedEntriesForOrderPage);
