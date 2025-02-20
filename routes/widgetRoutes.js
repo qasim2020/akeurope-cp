60f569921ee1b61518bc58e2317b9f327631d856
Qasim Ali <qasimali24@gmail.com>
2025-02-20 04:16:38 +0100

    removed un-nec links

--- routes/widgetRoutes.js
+++ routes/widgetRoutes.js
@@ -5,22 +5,23 @@ const { authenticate, authorize } = require('../modules/auth');
 const { notifyTelegram } = require('../modules/telegramBot');
 
 router.get('/widgets', authenticate, authorize('viewWidgets'), widgetController.widgets);
-// router.get('/payment-widget.js', authenticate, authorize('viewWidgets'), widgetController.widget);
 router.get('/script/:slug', widgetController.script);
 router.get('/overlay/:slug', widgetController.overlay);
-router.get('/get-country-list/:code', notifyTelegram, widgetController.getCountryList);
-// router.get('/store-prices', notifyTelegram, widgetController.storePrices);
-// router.get('/countries', notifyTelegram, widgetController.countries);
-router.get('/widget-new-order/:slug', notifyTelegram, widgetController.createNewOrder);
-router.post('/widget-update-order/:orderId/:slug', notifyTelegram, widgetController.updateOrder);
-router.get('/widget-order/:orderId', notifyTelegram, widgetController.getOrderData);
-router.get('/widget-render-entries/:orderId/:slug', notifyTelegram, widgetController.renderOrderEntries);
-router.get('/widget-render-no-order-total', notifyTelegram, widgetController.renderNoOrderTotal);
-router.get('/widget-render-no-order-entries/:slug', notifyTelegram, widgetController.renderNoOrderEntries);
+router.get('/get-country-list/:code', widgetController.getCountryList);
+router.get('/widget-new-order/:slug', widgetController.createNewOrder);
+router.post('/widget-update-order/:orderId/:slug', widgetController.updateOrder);
+router.get('/widget-order/:orderId', widgetController.getOrderData);
+router.get('/widget-render-entries/:orderId/:slug', widgetController.renderOrderEntries);
+router.get('/widget-render-no-order-total', widgetController.renderNoOrderTotal);
+router.get('/widget-render-no-order-entries/:slug', widgetController.renderNoOrderEntries);
 router.post('/create-payment-intent/:orderId/:slug', notifyTelegram, widgetController.createPaymentIntent);
 router.post('/create-setup-intent/:orderId/:slug', notifyTelegram, widgetController.createSetupIntent);
 router.post('/create-subscription/:orderId/:slug', notifyTelegram, widgetController.createSubscription);
 router.post('/create-one-time/:orderId/:slug', notifyTelegram, widgetController.createOneTime);
 router.post('/widget-order-customer/:orderId/:customerId', notifyTelegram, widgetController.linkOrderToCustomer);
+
+// router.get('/payment-widget.js', authenticate, authorize('viewWidgets'), widgetController.widget);
+// router.get('/store-prices', notifyTelegram, widgetController.storePrices);
+// router.get('/countries', notifyTelegram, widgetController.countries);
 
 module.exports = router;
